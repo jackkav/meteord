3b8c8978a8c319704c9bd1be10d0d1e0d648e7f2
Jack Kavanagh <jackkav@gmail.com>
2016-12-13 11:12:19 +0800

    using taobao npm and added echo
    added note of what is happening, because the output is hidden

--- base/scripts/run_app.sh
+++ base/scripts/run_app.sh
@@ -1,9 +1,13 @@
 set -e
 
+npm config set registry https://registry.npm.taobao.org
+npm config set disturl https://npm.taobao.org/dist
+
 if [ -d /bundle ]; then
   cd /bundle
   tar xzf *.tar.gz
   cd /bundle/bundle/programs/server/
+  echo "installing npm packages..."
   npm install --unsafe-perm &>/dev/null
   cd /bundle/bundle/
 else
